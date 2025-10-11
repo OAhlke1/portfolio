05141ad72c8332dcd4ff273e9a84409a7fefcc0a
OAhlke1 <oahlke1@gmail.com>
2025-10-11 11:00:59 +0200

    fixed height of lightbox in mobile view

--- src/app/work-together/work-together.component.ts
+++ src/app/work-together/work-together.component.ts
@@ -1,24 +1,58 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { NgClass } from "../../../node_modules/@angular/common/";
 import { PortfolioService } from '../shared/services/portfolio-service.service';
 import { ButtonComponent } from "../shared/buttons/button.component";
+import { FormsModule, NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'work-together',
   standalone: true,
-  imports: [NgClass, ButtonComponent],
+  imports: [NgClass, ButtonComponent, FormsModule],
   templateUrl: './work-together.component.html',
   styleUrl: './work-together.component.scss'
 })
 export class WorkTogether {
   @ViewChild('submitButton') submitButton!:ElementRef;
+  http = inject(HttpClient);
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  }
+  mailTest = false;
+
   constructor(public portService:PortfolioService) { }
 
-  clickButton(event:Event) {
-    this.submitButton.nativeElement.click();
-  }
+  post = {
+    endPoint: 'https://oscar-ahlke.de/angular/portfolio/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
 
-  submitFunction(event:Event) {
-    event.preventDefault();
+  onSubmit(ngForm: NgForm) {
+    console.log('läuft vielleicht', ngForm);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      console.log(ngForm);
+      ngForm.resetForm();
+    }
   }
 }
